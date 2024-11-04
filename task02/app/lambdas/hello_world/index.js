079a411a62ab4c7e1d1d7a6326beeac2e4c454d7
Mohammed Affan Khan Osmani <mohammedaffankhan_osmani@epam.com>
2024-11-04 14:18:45 +0530

    task02

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+    const path = event.path;
+    const method = event.httpMethod;
+
+    if (path === '/hello') {
+        const response = {
+            statusCode: 200,
+            message: "Hello from Lambda",
+        };
+        return response;
+    } else {
+        const response = {
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+        };
+        return response;
+    }
+};
