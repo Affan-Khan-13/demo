2af7228b9bd4a700772be1671f43135646e4aa5d
Mohammed Affan Khan Osmani <mohammedaffankhan_osmani@epam.com>
2024-11-06 17:34:41 +0530

    push

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,8 +1,11 @@
 exports.handler = async (event) => {
-    // TODO implement
+    for (const record of event.Records) {
+        const snsMessage = record.Sns.Message;
+        console.log('Received SNS message:', snsMessage);
+    }
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('SNS message logged successfully!'),
     };
     return response;
 };
