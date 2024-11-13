0524b14e76b5c650af1daa1ffa86075c63ffcecf
Mohammed Affan Khan Osmani <mohammedaffankhan_osmani@epam.com>
2024-11-13 11:35:13 +0530

    push08.6

--- task08/lambda-layer/nodejs/weather-api/fetchWeather.js
+++ task08/lambda-layer/nodejs/weather-api/fetchWeather.js
@@ -1,15 +1,20 @@
-const axios = require('axios');
-
 // URL for the Open-Meteo API
-const WEATHER_API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
+const WEATHER_API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
 
 async function fetchWeather() {
   try {
-    const response = await axios.get(WEATHER_API_URL);
-    return response.data; // Return weather data
+    const response = await fetch(WEATHER_API_URL);  // Fetch the weather data
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data: " + response.statusText);
+    }
+
+    const data = await response.json();  // Parse the response as JSON
+    console.log(data);  // Log the entire response
+    return data;  // Return weather data
+
   } catch (error) {
     console.error("Error fetching weather data:", error);
-    throw new Error("Failed to fetch weather data.");
+    // throw new Error("Failed to fetch weather data.");
   }
 }
 
